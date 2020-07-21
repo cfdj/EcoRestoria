163b7f80de0e9baf2f4d97da8831050fe0eccb45
cfdj43 <cd1097@york.ac.uk>
2020-07-21 02:18:53 +0100

    Hexagon grid and neighbours created

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    [SerializeField] int lineLength;
+    [SerializeField] int numLines;
+
+    [SerializeField] float xchange;
+    [SerializeField] float ychange;
+    [SerializeField] float linechange;
+    public List<Tile> GeneratableTiles;
+
+    private Tile[,] map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new Tile[lineLength, numLines];
+        GenerateGrid(0, 0);
+        setNeighbours();
+    }
+
+    void GenerateLine(float xStart, float yStart, int lineNum) //places a line of hexagons, line num tells it where in the map to place its line
+    {
+        for(int i = 0; i <lineLength; i++)
+        {
+            if (i % 2 == 0)  //only moving down every second hexagon
+            {
+                map[i, lineNum] = Instantiate(GeneratableTiles[0], new Vector3(xStart + i * xchange, yStart), Quaternion.identity);
+                
+            }
+            else
+            {
+                map[i, lineNum] = Instantiate(GeneratableTiles[0], new Vector3(xStart + i * xchange, yStart + ychange), Quaternion.identity);
+            }
+        }
+
+    }
+    void GenerateGrid (float xStart, float yStart)
+    {
+        for (int y = 0; y<numLines; y++)
+        {
+            GenerateLine(0, yStart + linechange * y, y);
+        }
+
+    }
+    void setNeighbours() //for each tile in map, set its neighbours, as setting neighbours works in both directions, each tile only needs to set the 3 neighbours below it.
+    {
+        for (int y = 0; y< numLines; y++)
+        {
+            for (int x = 0; x < lineLength; x++)
+            {
+                Debug.Log("x " + x + " y" + y);
+                if (x%2 == 0) //attaching x neighbours is different depending on if the tile is high or low
+                {
+                    if (x > 0)
+                    {
+                        map[x, y].AddNeighbour(map[x - 1, y], 4);
+                    }
+                    if (x < lineLength - 1)
+                    { 
+                        map[x, y].AddNeighbour(map[x + 1, y],2);
+                    }
+                }
+                else
+                {
+                    if ((x > 0)&& (y < numLines - 1))
+                    {
+                        map[x, y].AddNeighbour(map[x - 1, y + 1], 4);
+                    }
+                    if((x < lineLength - 1)&& (y<numLines-1))
+                    {
+                        map[x, y].AddNeighbour(map[x + 1, y + 1], 2);
+                    }
+                }
+
+                if (y < numLines-1)
+                {
+                    map[x, y].AddNeighbour(map[x, y + 1], 3);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Sprite sr; //the sprite displayed by this object
+    Tile[] neighbours; //going hexagonal so 0 is north, 3 is south, 5 is immediately next to north
+    public Contents contents; //what this sprite contains
+    public int height;
+
+    public Tile()
+    {
+        neighbours = new Tile[6];
+    }
+    public void AddNeighbour(Tile neighbour,int pos) //called by another tile to connect these as neighbours.
+    {
+        int npos = 0;
+        neighbours[pos] = neighbour;
+        switch (pos)
+        {
+            case 0:
+                npos = 3;
+                break;
+            case 1:
+                npos = 4;
+                break;
+            case 2:
+                npos = 5;
+                break;
+            case 3:
+                npos = 0;
+                break;
+            case 4:
+                npos = 1;
+                break;
+            case 5:
+                npos = 2;
+                break;
+        }
+        neighbour.neighbours[npos] = this;
+
+
+    }
+}
+
+
+
